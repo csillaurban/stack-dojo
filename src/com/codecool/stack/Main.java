9063e9bca827cebb59e3f9c8bc9fa26f84a9b115
Csilla Urban <csillaurban@gmail.com>
2019-05-01 17:56:00 +0200

    invoke methods in main

--- src/com/codecool/stack/Main.java
+++ src/com/codecool/stack/Main.java
@@ -1,5 +1,20 @@
 package com.codecool.stack;
 
-public class Main {
+import com.codecool.stack.stack.Stack;
 
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(4);
+
+        stack.pushToStack(3);
+        stack.pushToStack(2);
+        stack.pushToStack(5);
+        stack.pushToStack(7);
+        stack.printStack();
+        stack.pushToStack(12);
+        stack.popFromStack();
+        stack.popFromStack();
+        stack.printStack();
+        stack.peekFromStack();
+    }
 }
